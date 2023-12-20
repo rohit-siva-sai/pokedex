30c802448c71bc9d7f58c76debf1f943eb2cc755
rohit-siva-sai <106007387+rohit-siva-sai@users.noreply.github.com>
2023-12-20 21:56:02 +0530

    changes commit

--- app/page.js
+++ app/page.js
@@ -6,8 +6,10 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     {/* <MainModel/> */}
-      sjvshvhg
+     <picture>
+      <img src="./pokeball-icon.png" className='animate-spin' alt="" />
+     </picture>
+     <p className='font-semibold text-4xl text-center text-gray-700 mt-8'>POKE DEX</p>
     </main>
   )
 }
